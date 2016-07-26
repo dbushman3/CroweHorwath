75394aeadb4f178385c331169352d3f108c90b74
dbushman3 <dbushman3@gmail.com>
2016-07-26 10:30:45 -0500

    Initial Commit

---
+++ API/Classes/clsDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace API.Classes
+{
+    abstract class clsDB
+    {
+        private string m_strConn;
+
+        public clsDB()
+        {
+            this.m_strConn = ConfigurationManager.ConnectionStrings["MsSQLDBConnection"].ConnectionString;
+            //Code to set up database connection goes here.
+        }
+
+        public clsDB(string connString)
+        {
+            this.m_strConn = connString;
+            //Code to set up database connection goes here.
+        }
+        
+        abstract public void OpenConnection();
+        abstract public void CloseConnection();
+        abstract public long Execute(string SQL);
+    }
+}
